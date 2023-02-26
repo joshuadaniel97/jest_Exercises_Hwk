8b3147bb488c83efbf0755bbed0b3e4dc835e689
joshuadaniel97 <joshuano52@gmail.com>
2023-02-26 21:01:59 +0000

    adding homeworks

---
+++ jestHwk/functions.js
@@ -0,0 +1,5 @@
+let divide = (num1, num2) =>  num1 / num2 
+let multiply = (number1, number2) => number1 * number2
+let subtract = (numb1, numb2) => numb1 - numb2
+
+module.exports = {divide, multiply, subtract}
